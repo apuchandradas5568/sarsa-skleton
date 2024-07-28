faa9cc35ea3e56186674dd54d94d716238faf5d3
avdhesh0009 <avdheshkumar41237@gmail.com>
2024-07-28 20:33:47 +0530

    axios hooks conflicts resolved

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-<<<<<<< HEAD
-    baseURL: 'http://localhost:8000/api/v1'
-=======
     baseURL: 'http://localhost:8000/api/v1',
     withCredentials: true,
->>>>>>> origin/main
 })
 
 const useAxiosPublic = () => {
